691b2bc2d96cb369671afcc50e269ec5c1c9009f
Сергей <halygali@mail.ru>
2020-05-23 14:14:00 +0300

    Updates

---
+++ precache-manifest.7005d977e193ea1c4549817f49e27807.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b3b0b015960d3dbf05de109f49814c",
+    "url": "/lyubapidan/index.html"
+  },
+  {
+    "revision": "46968754258b4294ea01",
+    "url": "/lyubapidan/static/css/main.5dcb8622.chunk.css"
+  },
+  {
+    "revision": "bfcda9f7296edddba287",
+    "url": "/lyubapidan/static/js/2.5618404d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lyubapidan/static/js/2.5618404d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46968754258b4294ea01",
+    "url": "/lyubapidan/static/js/main.b7f92544.chunk.js"
+  },
+  {
+    "revision": "854b4a02ac934fad0dce",
+    "url": "/lyubapidan/static/js/runtime-main.9c9ed90c.js"
+  },
+  {
+    "revision": "48d09aeb42295836badbcb2d91c4d2ef",
+    "url": "/lyubapidan/static/media/LogoF1.48d09aeb.png"
+  },
+  {
+    "revision": "2952645f530ea5e19374d7f6020d4f61",
+    "url": "/lyubapidan/static/media/LogoF2.2952645f.jpg"
+  }
+]);
