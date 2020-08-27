f72b60c92d64343d413cacfc514dc83572e8b3d5
Сергей <halygali@mail.ru>
2020-08-27 07:22:57 +0300

    lesson 72 without typing

--- src/Components/Profile/ProfileInfo/profileInfo.tsx
+++ src/Components/Profile/ProfileInfo/profileInfo.tsx
@@ -2,21 +2,20 @@ import React from "react";
 import s from "./profileInfo.module.css";
 import LogoF2 from "../../../assets/Images/LogoF2.jpg";
 import Preloader from "../../common/Preloader/preloader";
-import Women from "../../../assets/Images/Women3.jpg"
+import ProfileStatus from "./ProfileStatus";
 
 const ProfileInfo = (props: any) => {
     if (!props.profile) {
-        // return <img className={s.NoPhoto} src={Women} alt='iso'/>
         return <Preloader/>
     }
     return (
         <div className={s.profileInfo}>
-            <div>
-                <img className={s.main} src={LogoF2} alt="iso"/>
-            </div>
+            {/*<div>*/}
+            {/*    <img className={s.main} src={LogoF2} alt="iso"/>*/}
+            {/*</div>*/}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large} alt="No Photo"/>
-                ava + description
+                <ProfileStatus status={'Privet Sergey!'}/>
             </div>
         </div>
     )
--- src/Components/Profile/profile.tsx
+++ src/Components/Profile/profile.tsx
@@ -3,17 +3,7 @@ import s from "./profile.module.css";
 import ProfileInfo from "./ProfileInfo/profileInfo";
 import MyPostsContainer from "./MyPosts/myPostsContainer";
 
-// export type ProfilePropsType = {
-    // posts: Array<PostType>
-    // newPostText: string
-    // addPost: (postMessage: string) => void
-    // updateNewPostText: (newText: string) => void
-    // dispatch: (action: ProfileActionsTypes) => void
-    // store: StoreType
-    // store: any
-// }
 
-// const Profile = (props: ProfilePropsType) => {
 const Profile = (props: any) => {
 
     return (
