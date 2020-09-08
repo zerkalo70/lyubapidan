be848d19730c91808f5306be506a8520940d25ff
Сергей <halygali@mail.ru>
2020-09-08 21:30:34 +0300

    lesson 93 without typing

--- src/Components/Users/users.tsx
+++ src/Components/Users/users.tsx
@@ -9,7 +9,7 @@ let Users = ({currentPage, totalUsersCount, pageSize, onPageChanged, users, ...p
         <div>
             <Paginator currentPage={currentPage}
                        onPageChanged={onPageChanged}
-                       totalUsersCount={totalUsersCount}
+                       totalItemsCount={totalUsersCount}
                        pageSize={pageSize}/>
             <div>
                 {
--- src/redux/usersReducer.ts
+++ src/redux/usersReducer.ts
@@ -12,10 +12,10 @@ const TOGGLE_IS_FOLLOWING_PROGRESS = "TOGGLE_IS_FOLLOWING_PROGRESS";
 
 let initialState: any = {
     users: [],
-    pageSize: 100,
+    pageSize: 10,
     totalUsersCount: 0,
     currentPage: 1,
-    isFetching: false,
+    isFetching: true,
     followingInProgress: []
 };
 
